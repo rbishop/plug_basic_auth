f495ebe06b9c81edfec77e653c14cecf3ae8009b
Richard Bishop <rbishop87@gmail.com>
2014-08-03 12:43:04 -0600

    Remove Cowboy as a dependency and delete empty config directory

--- mix.exs
+++ mix.exs
@@ -8,24 +8,11 @@ defmodule PlugBasicAuth.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: []]
   end
 
-  # Dependencies can be hex.pm packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
-    [{:cowboy, "~> 1.0.0"},
-     {:plug, "~> 0.5.0"}]
+    [{:plug, "~> 0.5.0"}]
   end
 end
