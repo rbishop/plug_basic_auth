2fbc4d5b1614c9afd1bbed9dcee85c9b5cd642ac
Richard Bishop <richard@rubiquity.com>
2015-05-08 17:12:55 -0700

    Bump library version to 0.3.1, republish to hex

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PlugBasicAuth.Mixfile do
 
   def project do
     [app: :plug_basic_auth,
-     version: "0.3.0",
+     version: "0.3.1",
      elixir: "~> 1.0.0",
      deps: deps,
      package: package,
