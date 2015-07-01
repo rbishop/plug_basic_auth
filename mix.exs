0415cdf921c9408b4ede059f41f54850aecf8df6
Richard Bishop <richard@rubiquity.com>
2015-07-01 08:20:04 -0600

    Bump to version 0.3.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PlugBasicAuth.Mixfile do
 
   def project do
     [app: :plug_basic_auth,
-     version: "0.3.2",
+     version: "0.3.3",
      elixir: "~> 1.0.0",
      deps: deps,
      package: package,
